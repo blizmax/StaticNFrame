f01151df3e2352169e42b1254c0c401f7980a3bc
xiaoyi445 <445267987@qq.com>
2018-05-04 21:05:28 +0800

    add build

---
+++ Server/Dependencies/build_dep.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Building dependencies...
+
+REM extract libevent libs
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+cd libevent
+nmake /f Makefile.nmake
+copy libevent.lib ..\\..\\Lib\\Debug\\ /Y
+copy libevent.lib ..\\..\\Lib\\Release\\ /Y
+copy libevent_core.lib ..\\..\\Lib\\Debug\\ /Y
+copy libevent_core.lib ..\\..\\Lib\\Release\\ /Y
+copy libevent_extras.lib ..\\..\\Lib\\Debug\\ /Y
+copy libevent_extras.lib ..\\..\\Lib\\Release\\ /Y
+cd ..
