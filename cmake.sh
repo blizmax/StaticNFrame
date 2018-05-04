73885700ea05d8dc0c73e04c2142cb65a0b73296
xiaoyi445 <445267987@qq.com>
2018-05-04 16:26:59 +0800

    modify cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,6 +1,21 @@
 cd Product
+mkdir build
 cd build
 rm * -rf
-cmake ../../
-make
+
+if [ $1 == "-R" ]
+then
+	cmake -DCMAKE_BUILD_TYPE=Release ../../
+else 
+	cmake -DCMAKE_BUILD_TYPE=Debug ../../
+fi
+
+if [ $2 == "-C" ]
+then
+	make clean
+	make -j4
+else
+	make -j4
+fi
+
 cd ../..
