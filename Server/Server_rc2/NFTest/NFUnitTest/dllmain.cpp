432ccd46c1f698cb476e0459a6948cf9e4301214
yigao <445267987@qq.com>
2018-05-08 21:52:07 +0800

    添加单元测试

---
+++ Server/Server_rc2/NFTest/NFUnitTest/dllmain.cpp
@@ -0,0 +1,7 @@
+#include "NFComm/NFCore/NFPlatform.h"
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+#pragma comment( lib, "ws2_32" )
+#pragma comment( lib, "NFCore.lib" )
+#pragma comment( lib, "gtest.lib" )
+#endif
