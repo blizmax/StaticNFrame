2bf5b9430a67dbfe49c97a27ffe2afd8d4c67de9
yigao <445267987@qq.com>
2018-05-09 12:24:04 +0800

    修改CMakeLists

--- Server/Dependencies/build_dep.sh
+++ Server/Dependencies/build_dep.sh
@@ -7,16 +7,16 @@ chmod a+x ./configure
 make clean
 make
 
-cp -R -f ./.libs/*.a ../../Lib/Debug/
-cp -R -f ./.libs/*.a ../../Lib/Release/
+cp -R -f ./.libs/*.a ../../../Product/Lib/Debug/
+cp -R -f ./.libs/*.a ../../../Product/Lib/Release/
 cd ../
 
 # compiling lua 
 cd lua
 make clean
 make all
-cp -R -f ./*.a ../../Lib/Debug/
-cp -R -f ./*.a ../../Lib/Release/
+cp -R -f ./*.a ../../../Product/Lib/Debug/
+cp -R -f ./*.a ../../../Product/Lib/Release/
 cd ../
 
 # compiling protobuf
@@ -26,12 +26,12 @@ chmod a+x ./configure
 make clean
 make
 
-cp -R -f ./src/.libs/*.a ../../Lib/Debug/
-cp -R -f ./src/.libs/*.a ../../Lib/Release/
+cp -R -f ./src/.libs/*.a ../../../Product/Lib/Debug/
+cp -R -f ./src/.libs/*.a ../../../Product/Lib/Release/
 
 
-cp -R -f ./src/.libs/*.so* ../../Lib/Debug/
-cp -R -f ./src/.libs/*.so* ../../Lib/Release/
+cp -R -f ./src/.libs/*.so* ../../../Product/Lib/Debug/
+cp -R -f ./src/.libs/*.so* ../../../Product/Lib/Release/
 
 cd ../
 
@@ -39,10 +39,10 @@ cd ../
 cd Theron
 make clean
 make library mode=debug boost=off c++11=on posix=on shared=on
-cp -r -f ./Lib/libtheron.a ../../Lib/Debug/
+cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Debug/
 make clean
 make library mode=release boost=off c++11=on posix=on shared=on
-cp -r -f ./Lib/libtheron.a ../../Lib/Release/
+cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Release/
 make clean
 cd ../
 
@@ -52,12 +52,12 @@ chmod a+x ./configure
 ./configure
 make clean
 make
-cp -R -f ./.libs/libtcmalloc.a ../../Lib/Debug/
-cp -R -f ./.libs/libtcmalloc.a ../../Lib/Release/
-cp -R -f ./.libs/libprofiler.a ../../Lib/Debug/
-cp -R -f ./.libs/libprofiler.a ../../Lib/Release/
-cp -R -f ./.libs/libtcmalloc_minimal.a ../../Lib/Debug/
-cp -R -f ./.libs/libtcmalloc_minimal.a ../../Lib/Release/
+cp -R -f ./.libs/libtcmalloc.a ../../../Product/Lib/Debug/
+cp -R -f ./.libs/libtcmalloc.a ../../../Product/Lib/Release/
+cp -R -f ./.libs/libprofiler.a ../../../Product/Lib/Debug/
+cp -R -f ./.libs/libprofiler.a ../../../Product/Lib/Release/
+cp -R -f ./.libs/libtcmalloc_minimal.a ../../../Product/Lib/Debug/
+cp -R -f ./.libs/libtcmalloc_minimal.a ../../../Product/Lib/Release/
 cd ../
 
 
