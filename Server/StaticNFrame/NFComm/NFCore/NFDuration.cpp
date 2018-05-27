dd5e5cb43ad7d64374ef5895cadc674e0c8bb157
yigao <445267987@qq.com>
2018-05-27 22:38:55 +0800

    static const

--- Server/StaticNFrame/NFComm/NFCore/NFDuration.cpp
+++ Server/StaticNFrame/NFComm/NFCore/NFDuration.cpp
@@ -9,10 +9,10 @@
 
 #include "NFDuration.h"
 
-const int64_t NFDuration::kNanosecond = 1LL;
-const int64_t NFDuration::kMicrosecond = 1000 * kNanosecond;
-const int64_t NFDuration::kMillisecond = 1000 * kMicrosecond;
-const int64_t NFDuration::kSecond = 1000 * kMillisecond;
-const int64_t NFDuration::kMinute = 60 * kSecond;
-const int64_t NFDuration::kHour = 60 * kMinute;
+int64_t NFDuration::kNanosecond = 1LL;
+int64_t NFDuration::kMicrosecond = 1000 * kNanosecond;
+int64_t NFDuration::kMillisecond = 1000 * kMicrosecond;
+int64_t NFDuration::kSecond = 1000 * kMillisecond;
+int64_t NFDuration::kMinute = 60 * kSecond;
+int64_t NFDuration::kHour = 60 * kMinute;
 
--- Server/StaticNFrame/NFComm/NFCore/NFDuration.h
+++ Server/StaticNFrame/NFComm/NFCore/NFDuration.h
@@ -22,12 +22,12 @@
 class NFDuration
 {
 public:
-	static const int64_t kNanosecond; // = 1LL
-	static const int64_t kMicrosecond;// = 1000
-	static const int64_t kMillisecond;// = 1000 * kMicrosecond
-	static const int64_t kSecond; // = 1000 * kMillisecond
-	static const int64_t kMinute; // = 60 * kSecond
-	static const int64_t kHour; // = 60 * kMinute
+	static int64_t kNanosecond; // = 1LL
+	static int64_t kMicrosecond;// = 1000
+	static int64_t kMillisecond;// = 1000 * kMicrosecond
+	static int64_t kSecond; // = 1000 * kMillisecond
+	static int64_t kMinute; // = 60 * kSecond
+	static int64_t kHour; // = 60 * kMinute
 public:
 	NFDuration();
 	explicit NFDuration(const struct timeval& t);
@@ -69,5 +69,126 @@ private:
 	int64_t ns_; // nanoseconds
 };
 
-#include "NFDuration.in.h"
+inline NFDuration::NFDuration()
+	: ns_(0)
+{
+}
+
+inline NFDuration::NFDuration(const struct timeval& t)
+	: ns_(t.tv_sec * kSecond + t.tv_usec * kMicrosecond)
+{
+}
+
+inline NFDuration::NFDuration(int64_t nanoseconds)
+	: ns_(nanoseconds)
+{
+}
+
+inline NFDuration::NFDuration(int nanoseconds)
+	: ns_(nanoseconds)
+{
+}
+
+inline NFDuration::NFDuration(double seconds)
+	: ns_((int64_t)(seconds * kSecond))
+{
+}
+
+inline int64_t NFDuration::Nanoseconds() const
+{
+	return ns_;
+}
+
+inline double NFDuration::Seconds() const
+{
+	return double(ns_) / kSecond;
+}
+
+inline double NFDuration::Milliseconds() const
+{
+	return double(ns_) / kMillisecond;
+}
+
+inline double NFDuration::Microseconds() const
+{
+	return double(ns_) / kMicrosecond;
+}
+
+inline double NFDuration::Minutes() const
+{
+	return double(ns_) / kMinute;
+}
+
+inline double NFDuration::Hours() const
+{
+	return double(ns_) / kHour;
+}
+
+inline bool NFDuration::IsZero() const
+{
+	return ns_ == 0;
+}
+
+inline struct timeval NFDuration::TimeVal() const
+{
+	struct timeval t;
+	To(&t);
+	return t;
+}
+
+inline void NFDuration::To(struct timeval* t) const
+{
+	t->tv_sec = (long)(ns_ / kSecond);
+	t->tv_usec = (long)(ns_ % kSecond) / (long)kMicrosecond;
+}
+
+inline bool NFDuration::operator<(const NFDuration& rhs) const
+{
+	return ns_ < rhs.ns_;
+}
+
+inline bool NFDuration::operator<=(const NFDuration& rhs) const
+{
+	return ns_ <= rhs.ns_;
+}
+
+inline bool NFDuration::operator>(const NFDuration& rhs) const
+{
+	return ns_ > rhs.ns_;
+}
+
+inline bool NFDuration::operator>=(const NFDuration& rhs) const
+{
+	return ns_ >= rhs.ns_;
+}
+
+inline bool NFDuration::operator==(const NFDuration& rhs) const
+{
+	return ns_ == rhs.ns_;
+}
+
+inline NFDuration NFDuration::operator+=(const NFDuration& rhs)
+{
+	ns_ += rhs.ns_;
+	return *this;
+}
+
+inline NFDuration NFDuration::operator-=(const NFDuration& rhs)
+{
+	ns_ -= rhs.ns_;
+	return *this;
+}
+
+inline NFDuration NFDuration::operator*=(int n)
+{
+	ns_ *= n;
+	return *this;
+}
+
+inline NFDuration NFDuration::operator/=(int n)
+{
+	ns_ /= n;
+	return *this;
+}
+
 
