6a5fef0245c3c868070ed1839b7d2c937a29b5f1
yigao <445267987@qq.com>
2018-05-27 23:59:00 +0800

    fuck me!

--- Server/StaticNFrame/NFComm/NFNetPlugin/NFDuration.cpp
+++ Server/StaticNFrame/NFComm/NFNetPlugin/NFDuration.cpp
@@ -9,14 +9,12 @@
 
 #include "NFDuration.h"
 
-#if NF_PLATFORM == NF_PLATFORM_WIN
 const int64_t NFDuration::kNanosecond = 1LL;
 const int64_t NFDuration::kMicrosecond = 1000 * kNanosecond;
 const int64_t NFDuration::kMillisecond = 1000 * kMicrosecond;
 const int64_t NFDuration::kSecond = 1000 * kMillisecond;
 const int64_t NFDuration::kMinute = 60 * kSecond;
 const int64_t NFDuration::kHour = 60 * kMinute;
-#endif
 
 NFDuration::NFDuration(): ns_(0)
 {
