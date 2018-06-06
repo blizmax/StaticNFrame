d6af80750c7bffbfd3b7e39dba3aac726d7adeb3
yigao <445267987@qq.com>
2018-06-07 00:45:42 +0800

    add mysqlclient.lib

--- Server/Dependencies/build_dep.bat
+++ Server/Dependencies/build_dep.bat
@@ -1,8 +1,14 @@
 @echo off
 echo Building dependencies...
 
-REM extract libevent libs
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
+cd mysql
+cmake -G "Visual Studio 14 2015 Win64"
+devenv.com libmysql.sln /build Debug
+cd ..
+
+
 cd libevent
 nmake /f Makefile.nmake
 copy libevent.lib ..\\..\\Lib\\Debug\\ /Y
