2107f4af289ae9d0214f41070b759459622a93ca
yi.gao <445267987@qq.com>
2018-11-29 16:53:23 +0800

    ok

--- Server/Dependencies/build_dep_mongo.bat
+++ Server/Dependencies/build_dep_mongo.bat
@@ -4,7 +4,7 @@ cd mongo
 mkdir cmake_build
 cd cmake_build
 cmake -G "Visual Studio 14 2015 Win64"  ..
-msbuild.exe /p:Configuration=RelWithDebInfo ALL_BUILD.vcxproj
+msbuild.exe /p:Configuration=Release ALL_BUILD.vcxproj
 
 cd ../..
 
