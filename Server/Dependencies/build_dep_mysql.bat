03597596223261374ee2dd8909c831d9fa614c00
yi.gao <445267987@qq.com>
2018-11-29 14:00:54 +0800

    ok

--- Server/Dependencies/build_dep_mysql.bat
+++ Server/Dependencies/build_dep_mysql.bat
@@ -4,7 +4,7 @@ echo Building dependencies...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 cd mysql
-cmake -G "Visual Studio 14 2015 Win64"
+cmake -G "Visual Studio 14 2015 Win64" 
 devenv.com libmysql.sln /build Release
 cd ..
 
