90e7fcd4ca3acf076d5ce366024285eeb064dde1
root <root@vultr.guest>
2018-12-22 10:16:47 +0800

    "modify"

--- Product/run/Debug/CreateScript.sh
+++ Product/run/Debug/CreateScript.sh
@@ -5,30 +5,30 @@ do_work()
 	ln -sdf $BASEPATH/NFPluginLoader $BASEPATH/$1$2
 	#$cp NFPluginLoader $1 -rf
 	#chmod a+x $1
-	cp AllServer.sh run$1$2.sh -rf
-	chmod a+x run$1$2.sh 
+	cp AllServer.sh $1$2_run.sh -rf
+	chmod a+x $1$2_run.sh 
 	cp AllServer_log.sh $1$2_log.sh -rf
 	chmod a+x $1$2_log.sh
-	sed -i "s/servername=NFPluginLoader/servername=$1$2/g" ./run$1$2.sh
-	sed -i "s/servertype=AllServer/servertype=$1/g" ./run$1$2.sh
-	sed -i "s/serverId=6/serverId=$2/g" ./run$1$2.sh
+	sed -i "s/servername=NFPluginLoader/servername=$1$2/g" ./$1$2_run.sh
+	sed -i "s/servertype=AllServer/servertype=$1/g" ./$1$2_run.sh
+	sed -i "s/serverId=6/serverId=$2/g" ./$1$2_run.sh
 
 	sed -i "s/AllServer6/AllServer$2/g" ./$1$2_log.sh
 	sed -i "s/AllServer/$1/g" ./$1$2_log.sh
 
 	touch Stop.sh
 	chmod a+x Stop.sh
-	echo "./run$1$2.sh ss" >> Stop.sh
+	echo "./$1$2_run.sh ss" >> Stop.sh
 
 	touch Start.sh
 	chmod a+x Start.sh
-	echo "./run$1$2.sh" >> Start.sh
+	echo "./$1$2_run.sh" >> Start.sh
 
 	touch Clear.sh
 	chmod a+x Clear.sh
 	echo "rm $1$2 -rf" >> Clear.sh
 	echo "rm $1$2_log.sh -rf" >> Clear.sh
-	echo "rm run$1$2.sh -rf" >> Clear.sh
+	echo "rm $1$2_run.sh -rf" >> Clear.sh
 }
 
 do_work MasterServer 1
