c0aec6b8d1678441bedf577941e9dd7297c6e07a
yi.gao <445267987@qq.com>
2018-12-28 19:42:32 +0800

    modify

---
+++ Product/run/ScriptModule/RebotServer/RebotPlayer.lua
@@ -0,0 +1,164 @@
+
+CreateClass("RebotPlayer")
+
+function RebotPlayer:Init(index)
+    self.name = "gaoyi"..index  --玩家名字 nickname
+    self.loginFinish = false
+    self.gameid = RebotServerModule.gameid
+    self.zoneid = 1
+    self.unigame_plat_key = RebotServerModule.openKey
+    self.unigame_plat_login = ""
+    self.sid = ""
+    self.uid = 0
+    self.unigame_plat_timestamp = os.time()
+    self.accountid = 0
+    self.gatewayurlws = ""
+    self.logintempid = 0
+    self.tokenid = 0
+    self.reconnect_logintempid = 0
+    self.reconnect_url = ""
+end
+
+function RebotPlayer:RequestZoneList()
+    local data = {}
+    data["do"] = "request-zone-list"
+    data["gameid"] = RebotServerModule.gameid
+
+    local param = { name = self.name }
+
+    unilight.HttpClientRequestPost(RebotServerModule.loginUrl, "httpLoginReturn", data, nil, param)
+end
+
+function RebotPlayer:HttpLoginReturn(respData)
+    self.zoneid = 11
+
+    local data = {}
+    data["do"] = "plat-token-login"
+    data["gameid"] = self.gameid
+    data["zoneid"] = self.zoneid
+    data["unigame_plat_timestamp"] = os.time()
+    data["data"] = { gameid = self.gameid,}
+    data["data"]["platinfo"] = {
+        aaaa = 123,
+        sign = RebotServerModule.openKey,
+        account = self.name,
+        platid = RebotServerModule.platid,
+        gameid = self.gameid,
+    }
+
+    local param = { name = self.name }
+
+    unilight.HttpClientRequestPost(RebotServerModule.loginUrl, "plat_token_login_return", data, nil, param)
+end
+
+function RebotPlayer:PlatTokenLoginReturn(respData)
+    self.unigame_plat_key = respData.data.unigame_plat_key
+    self.unigame_plat_login = respData.data.unigame_plat_login
+    self.sid = respData.data.sid
+    self.uid = tonumber(respData.data.uid)
+    self.unigame_plat_timestamp = os.time()
+
+    RebotServerModule.rebotUidMap[self.uid] = self
+
+    local data = {}
+    data["do"] = "request-select-zone"
+    data["data"] = {}
+    data["gameid"] = self.gameid
+    data["zoneid"] = self.zoneid
+    data["uid"] = tostring(self.uid)
+    data["unigame_plat_login"] = self.unigame_plat_login
+    data["unigame_plat_timestamp"] = self.unigame_plat_timestamp
+
+    local strJson = table2json(data)
+
+    local sel_url = RebotServerModule.loginUrl .. "?unigame_plat_sign=" .. unilight.GetMD5(strJson..self.unigame_plat_timestamp..self.unigame_plat_key)
+
+    local param = { name = self.name }
+
+    unilight.HttpClientRequestPost(sel_url, "select_zone_return", data, nil, param)
+end
+
+function RebotPlayer:SelectZoneReturn(respData)
+    self.accountid = respData.data.accountid
+    self.gatewayurlws = respData.data.gatewayurlws .. "/json"
+    self.gatewayurltcp = respData.data.gatewayurltcp
+    self.logintempid = respData.data.logintempid;
+    self.logintempid = 0
+    self.tokenid = respData.data.tokenid;
+    self.reconnect_logintempid = self.logintempid
+
+    self.reconnect_url = self.gatewayurlws
+
+    self.loginFinish = true
+
+    self.unlinkid = TcpClient:addWebServer(NF_SERVER_TYPES.NF_ST_PROXY, self.gatewayurlws)
+    if self.unlinkid ~= 0 then
+        RebotServerModule.rebotUnlinkIdMap[self.unlinkid] = self
+    end
+end
+
+function RebotPlayer:Connect()
+    self:LoginTokenLogin()
+    self:DoRequestPlayerBaseInfo()
+    self:RequestTravelData()
+    self:RequestGuideInfo()
+end
+
+function RebotPlayer:LoginTokenLogin()
+    local timestamp = os.time()
+    local tmp = tostring(self.accountid) .. tostring(self.reconnect_logintempid) .. tostring(timestamp) .. self.unigame_plat_key
+
+    local data = {
+        version = 20160805,
+        gameid = self.gameid,
+        zoneid = self.zoneid,
+        accountid = self.accountid,
+        logintempid = self.logintempid,
+        timestamp = timestamp,
+        compress = "",
+        tokenmd5 = unilight.GetMD5(tmp),
+        cmd_name = "Pmd.UserLoginTokenLoginUserPmd_C",
+    }
+
+    TcpClient.sendMsgByServerId(data, self.unlinkid)
+end
+
+function RebotPlayer:SendMessage(data)
+    if type(data) == "table" then
+        data = table2json(data)
+    end
+    local strMsg = {
+        cmd_name = "Pmd.UserJsMessageForwardUserPmd_CS",
+        msg = data,
+    }
+
+    TcpClient.sendMsgByServerId(strMsg, self.unlinkid)
+end
+
+function RebotPlayer:DoRequestPlayerBaseInfo()
+    local data = { }
+    data["do"] = "Cmd.UserInfoSynLobbyCmd_C"
+    data["data"] = {}
+
+    self:SendMessage(data)
+end
+
+function RebotPlayer:RequestTravelData()
+    local data = { }
+    data["do"] = "Cmd.GetUserTravelInfo_C"
+    data["data"] = {}
+
+    self:SendMessage(data)
+end
+
+function RebotPlayer:RequestGuideInfo()
+    local data = { }
+    data["do"] = "Cmd.GetGuideInfoCmd_C"
+    data["data"] = {}
+
+    self:SendMessage(data)
+end
+
+function RebotPlayer:DisConnect()
+
+end
