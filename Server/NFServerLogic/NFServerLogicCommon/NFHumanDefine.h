26b515bbbf2b2ca5724d89cb690b624a859c7b75
xiaoyi445 <445267987@qq.com>
2019-04-07 20:19:43 +0800

    human

---
+++ Server/NFServerLogic/NFServerLogicCommon/NFHumanDefine.h
@@ -0,0 +1,15 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFHumanDefine.h
+//    @Author           :    GaoYi
+//    @Date             :    2019/04/7
+//    @Email			:    445267987@qq.com
+//    @Module           :    NFHumanDefine
+//
+// -------------------------------------------------------------------------
+
+#pragma once
+
+#include "NFMessageDefine/NFMsgDefine.h"
+
+#include "NFMessageDefine/st_human.pb.h"
+#include "NFMessageDefine/st_human_packet_code.pb.h"
