085a7597bbde236bad0442253701e0afcdde2f87
xiaoyi445 <445267987@qq.com>
2019-05-07 00:18:22 +0800

    "modify"

--- Server/Dependencies/build_dep_all.sh
+++ Server/Dependencies/build_dep_all.sh
@@ -9,6 +9,10 @@ cp -rf libcrypto.a ../../../Product/Lib/Debug/
 cp -rf libcrypto.a ../../../Product/Lib/Release/
 cp -rf libssl.a ../../../Product/Lib/Debug/
 cp -rf libssl.a ../../../Product/Lib/Release/
+cp -rf libcrypto.so.1.1 ../../../Product/run/Dynamic_Debug/libcrypto.so
+cp -rf libssl.so.1.1 ../../../Product/run/Dynamic_Debug/libssl.so
+cp -rf libcrypto.so.1.1 ../../../Product/run/Dynamic_Release/libcrypto.so
+cp -rf libssl.so.1.1 ../../../Product/run/Dynamic_Release/libssl.so
 cd ..
 
 #compiling mysql
@@ -18,6 +22,9 @@ make
 
 cp -R -f ./libmysql/libmysqlclient.a ../../../Product/Lib/Debug/
 cp -R -f ./libmysql/libmysqlclient.a ../../../Product/Lib/Release/
+cp -R -f ./libmysql/libmysqlclient.so.18.4. ../../../Product/run/Dynamic_Debug/libmysqlclient.so
+cp -R -f ./libmysql/libmysqlclient.so.18.4. ../../../Product/run/Dynamic_Release/libmysqlclient.so
+
 cd ..
 
 # compiling libevent
@@ -30,6 +37,8 @@ cd ..
 
 cp -R -f ./build/lib/*.a ../../../Product/Lib/Debug/
 cp -R -f ./build/lib/*.a ../../../Product/Lib/Release/
+cp -R -f ./build/lib/libevent_core.so.2.1.8 ./build/lib/libevent.so.2.1.8 ./build/lib/libevent_extra.so.2.1.8 ../../../Product/run/Dynamic_Debug/
+cp -R -f ./build/lib/libevent_core.so.2.1.8 ./build/lib/libevent.so.2.1.8 ./build/lib/libevent_extra.so.2.1.8 ../../../Product/run/Dynamic_Release/
 #cp -R -f ./build/include ./linux/
 cd ../
 
@@ -45,6 +54,8 @@ cd ../../
 
 cp -R -f ./cmake/build/*.a ../../../Product/Lib/Debug/
 cp -R -f ./cmake/build/*.a ../../../Product/Lib/Release/
+cp -R -f ./cmake/build/libprotobuf.so.3.6.1 ../../../Product/run/Dynamic_Debug/
+cp -R -f ./cmake/build/libprotobuf.so.3.6.1 ../../../Product/run/Dynamic_Release/
 
 cd ../
 
@@ -52,8 +63,10 @@ cd ../
 cd Theron
 make library mode=debug boost=off c++11=on posix=on shared=on
 cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Debug/
+cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Dynamic_Debug/
 make library mode=release boost=off c++11=on posix=on shared=on
 cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Release/
+cp -r -f ./Lib/libtheron.a ../../../Product/Lib/Dynamic_Release/
 cd ../
 
 
@@ -69,6 +82,10 @@ cp -rf src/libmongoc/libmongoc-static-1.0.a ../../../../Product/Lib/Debug/
 cp -rf src/libmongoc/libmongoc-static-1.0.a ../../../../Product/Lib/Release/
 cp -rf src/libbson/libbson-static-1.0.a ../../../../Product/Lib/Debug/
 cp -rf src/libbson/libbson-static-1.0.a ../../../../Product/Lib/Release/
+cp -rf src/libmongoc/libmongoc-1.0.so.0.0.0 ../../../../Product/run/Dynamic_Debug/
+cp -rf src/libbson/libbson-1.0.so.0.0.0 ../../../../Product/run/Dynamic_Debug/
+cp -rf src/libmongoc/libmongoc-1.0.so.0.0.0 ../../../../Product/run/Dynamic_Release/
+cp -rf src/libbson/libbson-1.0.so.0.0.0 ../../../../Product/run/Dynamic_Release/
 cd ../../
 
 
