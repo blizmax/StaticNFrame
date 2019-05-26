1a1c7535d6b6c5e41c1d3eb21c9e41509a19d0c3
yigao <445267987@qq.com>
2019-05-27 00:15:01 +0800

    lua 5.1注册失败

---
+++ Product/run/ScriptModule/trdlib/Init.lua
@@ -0,0 +1,14 @@
+
+
+--require("./external/TimeUtils.lua")
+require('trdlib.external.TimeUtils')
+require("trdlib.external.functions")
+require("trdlib.external.luaDump")
+
+luajson = require("trdlib.external.json")
+redisConnect = require("trdlib.external.redisConnect")
+
+mysqlConnect = require("trdlib.external.mysqlConnect")
+
+require("trdlib.libprotobuf.protobuf")
+
---
+++ Product/run/ScriptModule/trdlib/external/mysqlConnect.lua
@@ -0,0 +1,90 @@
+
+if g_platfrom == 'luaself' then
+    require "luasql.mysql"
+end
+
+local mysqlConnect = class("mysqlConnect")
+
+function mysqlConnect:ctor()
+	LogFile("info",'mysql connect start:dbtype='..g_dbtype..',dbuser='..g_dbUser..',dbhost='..g_dbHost..',dbPort='..g_dbPort)
+	self.m_env = luasql.mysql()
+	self.m_connect = assert(self.m_env:connect(g_dbtype, g_dbUser, g_dbPassword, g_dbHost, g_dbPort))
+	self.m_connect:execute[[set names utf8]]
+	self.m_connect:execute("use "..g_dbDatabase)
+
+	LogFile("info",'mysql connect success')
+end
+
+
+function mysqlConnect:reConnect()
+	--body
+	LogFile("info",'mysql reconnect dbtype='..g_dbtype..',dbuser='..g_dbUser..',dbhost='..g_dbHost..',dbPort='..g_dbPort)
+
+	self:release()
+
+	self.m_env = luasql.mysql()
+	self.m_connect = assert(self.m_env:connect(g_dbtype, g_dbUser, g_dbPassword, g_dbHost, g_dbPort))
+	self.m_connect:execute[[set names utf8]]
+	self.m_connect:execute("use "..g_dbDatabase)	
+end
+
+function mysqlConnect:executeQuery(sql)
+	--LogFile("sql",sql)
+	
+	self.cursor = self.m_connect:execute(sql)
+
+	if self.cursor == nil then
+		self:reConnect()
+		--防止超时链接断掉出现
+		self.cursor = self.m_connect:execute(sql)
+	end
+
+	return self.cursor
+end
+
+function mysqlConnect:executeUpdate(sql)
+	--LogFile("sql",sql)
+	
+	local res = self.m_connect:execute(sql)
+	
+
+	if self.cursor == nil then
+		self:reConnect()
+		--防止超时链接断掉出现
+		self.cursor = self.m_connect:execute(sql)
+	end
+
+	return res
+end
+
+function mysqlConnect:execute(sql)
+	--LogFile("sql",sql)
+	--print("sql="..sql)
+	local res = self.m_connect:execute(sql)
+	
+	if self.cursor == nil then
+		self:reConnect()
+		--防止超时链接断掉出现
+		self.cursor = self.m_connect:execute(sql)
+	end
+
+	return res
+end
+
+
+function mysqlConnect:release()
+
+	self.m_connect:close()
+	self.m_env:close()
+
+end
+function mysqlConnect:numrows( )
+	return self.cursor:numrows()
+	-- body
+end
+
+function mysqlConnect:fetch(...)
+	return self.cursor:fetch(...)
+end
+
+return mysqlConnect
