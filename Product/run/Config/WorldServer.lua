787123109718efb6dc09bc41cb390584915876ea
xiaoyi445 <445267987@qq.com>
2019-09-02 09:57:11 +0800

    6003

--- Product/run/Config/WorldServer.lua
+++ Product/run/Config/WorldServer.lua
@@ -47,7 +47,7 @@ function InitGameServer()
 			ServerType = NF_ST_PROXY,
 			ServerId = 6,			--对每一个服务器来说都是唯一的， 应用程序需要通过这个ServerId才能知道需要加载的配置是他
 			ServerIp = MasterIP,
-			ServerPort = 6103,
+			ServerPort = 6003,
 			MaxConnectNum = 100,
 			WorkThreadNum = 5,
 			Security = false,
