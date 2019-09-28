10814e88dbb0184f241d34eddde09479beee5c2d
xiaoyi445 <445267987@qq.com>
2019-09-28 10:47:29 +0800

    loaderhelp

--- Product/run/Debug/runAll.bat
+++ Product/run/Debug/runAll.bat
@@ -1,7 +1,6 @@
 start NFPluginLoader --Server=MasterServer --ID=1 --Path=../Config --LuaScript=../ScriptModule
 start NFPluginLoader --Server=LoginServer --ID=2 --Path=../Config --LuaScript=../ScriptModule
-#start NFPluginLoader --Server=WorldServer --ID=3 --Path=../Config --LuaScript=../ScriptModule
+start NFPluginLoader --Server=WorldServer --ID=3 --Path=../Config --LuaScript=../ScriptModule
 start NFPluginLoader --Server=GameServer --ID=4 --Path=../Config --LuaScript=../ScriptModule
 #start NFPluginLoader --Server=GameServer --ID=5 --Path=../Config --LuaScript=../ScriptModule
-start NFPluginLoader --Server=ProxyServer --ID=6 --Path=../Config --LuaScript=../ScriptModule
-#start NFPluginLoader --Server=ProxyServer --ID=7 --Path=../Config --LuaScript=../ScriptModule                        
+start NFPluginLoader --Server=ProxyServer --ID=6 --Path=../Config --LuaScript=../ScriptModule                       
