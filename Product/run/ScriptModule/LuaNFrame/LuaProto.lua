1a92efc308a4a084e94f6cc07452d121674a90ad
xiaoyi445 <445267987@qq.com>
2019-12-02 13:03:51 +0800

    luaprotoc

---
+++ Product/run/ScriptModule/LuaNFrame/LuaProto.lua
@@ -0,0 +1,8 @@
+LuaProto = LuaProto or {}
+
+LuaProto.pb     = require "pb"
+LuaProto.pb_io   = require "pb_io"
+LuaProto.pb_buffer = require "pb_buffer"
+LuaProto.pb_slice  = require "pb_slice"
+LuaProto.pb_conv   = require "pb_conv"
+LuaProto.protoc = require "protoc"
